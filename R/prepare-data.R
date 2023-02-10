2c6cf4e5f6664cacf7e07aaea6cffe4c1fd7aaca
max607 <63913351+max607@users.noreply.github.com>
2023-02-10 10:07:13 +0200

    final status

--- R/prepare-data.R
+++ R/prepare-data.R
@@ -13,24 +13,30 @@ vars_cat <- c("education", "gender", "urban", "engnat", "hand", "religion", "ori
 dt_bigf[, c("country", "education", "urban", "gender", "engnat", "hand", "religion",
             "orientation", "race", "voted", "married", "os", "browser") :=
           .(factor(country),
-            factor(education, labels = c("le_hs", "hs", "uni_degree", "grad_degree"), exclude = 0),
-            factor(urban, labels = c("rural", "suburban", "urban"), exclude = 0),
-            factor(gender, labels = c("male", "female", "other"), exclude = 0),
-            factor(engnat, labels = c("yes", "no"), exclude = 0),
-            factor(hand, labels = c("right", "left", "both"), exclude = 0),
-            factor(religion, labels = c("agnostic", "atheist", "buddhist", "catholic", "mormon",
-                                        "protestant", "christian_other", "hindu",
-                                        "jewish", "muslim", "sikh", "other"), exclude = 0),
-            factor(orientation, labels = c("heterosexual", "bisexual", "homosexual", "asexual",
-                                           "other"), exclude = 0),
-            factor(race, labels = c("misc", "asian", "arab", "black", "other"), exclude = 0),
-            factor(voted, labels = c("no", "yes"), exclude = 0),
-            factor(married, labels = c("no", "yes", "divorced"), exclude = 0),
+            factor(education, levels = 1:4, labels = c("le_hs", "hs", "uni_degree", "grad_degree"),
+                   exclude = 0),
+            factor(urban, levels = 1:3, labels = c("rural", "suburban", "urban"), exclude = 0),
+            factor(gender, levels = 1:3, labels = c("male", "female", "other"), exclude = 0),
+            factor(engnat, levels = 2:1, labels = c("no", "yes"), exclude = 0),
+            factor(hand, levels = 1:3, labels = c("right", "left", "both"), exclude = 0),
+            factor(religion, levels = 1:12,
+                   labels = c("agnostic", "atheist", "buddhist", "catholic", "mormon", "protestant",
+                              "christian_other", "hindu", "jewish", "muslim", "sikh", "other"),
+                   exclude = 0),
+            factor(orientation, levels = 1:5,
+                   labels = c("heterosexual", "bisexual", "homosexual", "asexual", "other"),
+                   exclude = 0),
+            factor(race, levels = c(4, 1:3, 5),
+                   labels = c("misc", "asian", "arab", "black", "other"), exclude = 0),
+            factor(voted, levels = 2:1, labels = c("no", "yes"), exclude = 0),
+            factor(married, levels = 1:3, labels = c("no", "yes", "divorced"), exclude = 0),
             factor(operatingsystem), factor(browser))
 ]
 
+# Identifying wrong values -------------------------------------------------------------------------
+
 # family size wrong - women with most children had 44
-dt_bigf[familysize > 44 | familysize == 0, familysize := NA]
+dt_bigf[familysize > 20 | familysize == 0, familysize := NA]
 
 # age wrong
 dt_bigf[age > 99, age := NA]
@@ -50,18 +56,18 @@ dt_bigf[, eloquence := rowSums(.SD), .SDcols = patterns("^VCL")]
 # start at zero
 dt_bigf[, (names_o) := .SD - 1, .SDcols = names_o]
 
-p_corr1 <- cor(dt_bigf[, .SD, .SDcols = patterns("^O")], use = "pairwise.complete.obs") %>%
-  ggcorrplot::ggcorrplot(method = "square", type = "lower", lab = TRUE, digits = 1,
-                         ggtheme = ggplot2::theme_bw, outline.color = "black",
-                         colors = c("#1984c5", "#e2e2e2", "#c23728"), show.legend = FALSE)
+# p_corr1 <- cor(dt_bigf[, .SD, .SDcols = patterns("^O")], use = "pairwise.complete.obs") %>%
+#   ggcorrplot::ggcorrplot(method = "square", type = "lower", lab = TRUE, digits = 1,
+#                          ggtheme = ggplot2::theme_bw, outline.color = "black",
+#                          colors = c("#1984c5", "#e2e2e2", "#c23728"), show.legend = FALSE)
 
 openness_negation <- paste0("O", c(2, 4, 6))
 dt_bigf[, (openness_negation) := 4 - .SD, .SDcols = openness_negation]
 
-p_corr2 <- cor(dt_bigf[, .SD, .SDcols = patterns("^O")], use = "pairwise.complete.obs") %>%
-  ggcorrplot::ggcorrplot(method = "square", type = "lower", lab = TRUE, digits = 1,
-                         ggtheme = ggplot2::theme_bw, outline.color = "black",
-                         colors = c("#1984c5", "#e2e2e2", "#c23728"), show.legend = FALSE)
+# p_corr2 <- cor(dt_bigf[, .SD, .SDcols = patterns("^O")], use = "pairwise.complete.obs") %>%
+#   ggcorrplot::ggcorrplot(method = "square", type = "lower", lab = TRUE, digits = 1,
+#                          ggtheme = ggplot2::theme_bw, outline.color = "black",
+#                          colors = c("#1984c5", "#e2e2e2", "#c23728"), show.legend = FALSE)
 
 # numbers of missing answers per person (all values are missing for two obs)
 dt_bigf[, O_NA := rowSums(is.na(.SD)), .SDcols = names_o]
